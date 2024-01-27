072fc2fe103de523a5a5df098aaa69c340a0e74e
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-27 15:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete this js code
 function Person(name, age) {}
 
+Person.prototype.greet=function(){
+	let str=`hello my name is${this.name} , I am ${this.age} yerars old.`
+}
 function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
